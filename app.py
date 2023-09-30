bc044296c0e68cee97251869238ea54e57060f1e
pedroFelipe11 <githubpedro08@gmail.com>
2023-09-30 11:20:00 -0300

    funçoes

---
+++ app.py
@@ -0,0 +1,57 @@
+#-----------Função Calcular media----------------------------------------------------------------------------------------
+'''Função que recebe  a media do usuario como parâmetro,calcula o resultado e informa se passou ou não '''
+# def calculate_mean():
+#     print('Digite a nota da primeira prova')
+#     number1 = float(input('-> '))
+#     print('Digite a nota da segunda prova')
+#     number2 =float(input('-> ')) 
+
+#    mean = (number1 + number2)/2
+#     if mean >= 10:
+#         print('você passou')
+#     else:
+#         print('Você perdeu')
+#     return mean
+
+# print(calculate mean())
+
+
+#----------Função Calcular media 2-----------------------------------------------------------------------------------------
+
+# def calculate_mean2(n1: float, n2 : float , n3: float):
+'''Funcão  que calcula a mèdia de três notas,obrigando  receber parâmetros'''
+    # mean = (n1 + n2 + n3 ) /3
+    # return mean
+
+# print(calculate_mean(5,7,9))
+
+
+#-------Função Saudaçao------------------------------------------------------------------------------
+
+# def (name: str) -> str:
+'''Função que recebe um nome como parâmetro e retorna uma saudação para ele'''
+    # return f"Ola,{name}"
+
+# print(greetings('Pedro'))
+  
+
+#-----Função que calcula o IMC-------------------------------------------------------------------------
+
+# def imc(w: float , h: float):
+'''Função que recebe como parâmetro altura e peso do ususario para calcular seu imc'''
+#     imc = (w/h**2)
+#     return imc
+# peaple = 0
+
+
+# while True:
+#     print('Seja bem vindo ao calculo de imc ')
+#     name = input('Digite seu nome: ')
+#     hieght  = float(input('Digite sua altura: '))
+#     weight = float(input('Digite seu peso: '))
+#     peaple_imc = imc(weight,height)
+#     print(f'{name} tem {peaple_imc:.2f} de imc')
+#     peaples += 1
+#     if peaple == 4:
+#         print('Programa finalizado')
+#         break
